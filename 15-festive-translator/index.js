2c8ab0defaf170448541bc6029d9aeb6097b1c22
Daryl Nauman <darylnauman@gmail.com>
2021-12-15 10:24:21 -0500

    posting my initial solution for the December 15, 2021 (Day 15) challenge

---
+++ 15-festive-translator/index.js
@@ -0,0 +1,34 @@
+const languageSelector = document.getElementById("language-selector")
+const greetingDisplay = document.getElementById("greeting-text")
+
+languageSelector.addEventListener("change", translate)
+
+const greetingsArr = [
+    {
+        language: "English",
+        greeting: "Merry Christmas!"
+    },
+    {
+        language: "Spanish",
+        greeting: "Feliz Navidad!"
+    },
+    {
+        language: "Ukrainian",
+        greeting: "щасливого Різдва!"
+    },
+    {
+        language: "Welsh",
+        greeting: "Nadolig Llawen!"
+    }
+]
+
+function translate(){
+    // Task: 
+    // - Write a function to display the correct greeting when a language is selected.
+    const found = greetingsArr.find( item => item.language === languageSelector.value);
+    greetingDisplay.innerText = found.greeting;
+}
+
+// Stretch goals:
+// - Animate the message switch.
+// - Add the option to switch the greeting, for example also have "And a Happy New Year!".
